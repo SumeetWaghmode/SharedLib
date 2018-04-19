9cf3cfb1811bd91fb3bc8ff62a5467dc24829b63
SumeetWaghmode <sumeet.waghmode@gmail.com>
2018-04-19 17:57:28 +0530

    Edited sanePWD

---
+++ vars/buildMaven.groovy
@@ -0,0 +1,26 @@
+/**
+ * This map contains the nodes and TC_ROOT/DATA which should be used for each release
+ * @param release Name of the release
+ */
+def call(String workspaceLocation) 
+{
+	try {
+			bat """@echo on
+					mvn install"""
+
+			//archiveArtifacts artifacts: 'client_customization/client_binaries/**', fingerprint: true
+			//stash name: "winJars", includes: "client_customization/client_binaries/**"
+			//reportTestResult {
+			//	classname = 'com.siemens.plm2020.build'
+			//	testname = 'buildWinJars'
+			//}
+		} catch (Exception e) {
+			println "Error is --> " + e.getMessage()
+			reportTestResult {
+				classname = 'com.siemens.plm2020.build'
+				testname = 'buildWinJars'
+				failure = e.getMessage()
+			}
+			return e
+		}
+}
