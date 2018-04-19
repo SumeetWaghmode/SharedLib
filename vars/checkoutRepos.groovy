99927a0de1dd67de16448c47c7370e30f7c61a70
SumeetWaghmode <sumeet.waghmode@gmail.com>
2018-04-19 16:16:21 +0530

    Edited sanePWD

---
+++ vars/checkoutRepos.groovy
@@ -0,0 +1,48 @@
+def call(String workspace) {
+	echo workspace
+	dir(workspace) {
+		if (isUnix()) {
+			vcsResult = checkout changelog: false, poll: false,
+			scm: [$class                           : 'GitSCM',
+				branches                         : [[name: refspec]],
+				doGenerateSubmoduleConfigurations: false,
+				extensions                       : [
+					[$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [
+							[path: '/bmide'],
+							[path: '/plm2020_auto_build/windows'],
+							[path: '/server_customization/src']
+						]]],
+				submoduleCfg                     : [],
+				userRemoteConfigs                : [[url: repoURL]]
+			]
+
+			// Clean old tmp files
+			def gitPath = tool('Git')
+			sh "${gitPath} clean -fd -e '**/history.xml' -e '**/obj' -e '**/lnx'"
+
+			// Write the commit hash for the package build
+			writeFile file: 'gitCommitHash.txt', text: "${vcsResult['GIT_COMMIT']}"
+			archiveArtifacts artifacts: 'gitCommitHash.txt', fingerprint: true
+			stash name: "lnxGitCommithash", includes: "gitCommitHash.txt"
+
+		} else {
+			// Is windows
+
+			vcsResult = checkout changelog: false, poll: false,
+			scm: [$class                           : 'GitSCM',
+				branches                         : [[name: '*/master']],
+				doGenerateSubmoduleConfigurations: false,
+				extensions                       : [],
+				submoduleCfg                     : [],
+				userRemoteConfigs                : [[credentialsId: '5b9ca0cf-ce16-445b-96d7-1fa7466ce7b1', url: 'https://github.com/SumeetWaghmode/ExportXLSservice.git']]
+			]
+
+			writeFile file: 'gitCommitHash.txt', text: "${vcsResult['GIT_COMMIT']}"
+			/*archiveArtifacts artifacts: 'gitCommitHash.txt', fingerprint: true
+			stash name: "winGitCommithash", includes: "gitCommitHash.txt" */
+		}
+
+	}
+}
+
+return this
