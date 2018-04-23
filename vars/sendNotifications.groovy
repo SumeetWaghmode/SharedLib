07403740a8e7d0948f2c7438572033cc31ace779
SumeetWaghmode <sumeet.waghmode@gmail.com>
2018-04-23 11:44:01 +0530

    Edited sendNotification

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -16,9 +16,9 @@ def call(config)
 		{
 			echo 'Current Build is SUCCESSFul'
 			emailext attachLog: true, body: '\'\'\'Build Failed. we are testing the email configuration for failure cases!! </BR></BR>This will be updated with more details soon. </BR></BR>*********** Please DO NOT reply to this email ***********\'\'\'', subject: 'AutoBuild', to: 'sumeet.waghmode@intelizign.com'
-			/*ansiColor('xterm') {
+			ansiColor('xterm') {
 				println "\033[1;32m Current Build Result:: ${currentBuild.currentResult} \033[0m"
-			}
+			}/*
 			emailext body: '''${SCRIPT, template="groovy-html.template"}''',
 			mimeType: 'text/html',
 			subject: 'Build Success:'+subjectContents,
